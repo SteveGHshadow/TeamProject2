d3cf8b503c66aea6a67cb1df54340c51831ba7a4
SteveGHshadow <steve.itunesjp@hotmail.com>
2022-11-20 20:53:21 +0000

    Changed the nav bar and removed nav from dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,35 +6,35 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex ">
-            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-nav-link>
-            <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
-                {{ __('home') }}
-            </x-nav-link>
-            <x-nav-link :href="route('items')" :active="request()->routeIs('items')">
-                {{ __('Items') }}
-            </x-nav-link>
-            <x-nav-link :href="route('contactUs')" :active="request()->routeIs('contactUs')">
-                {{ __('Contact Us') }}
-            </x-nav-link>
-            <x-nav-link :href="route('aboutUs')" :active="request()->routeIs('aboutUs')">
-                {{ __('About Us') }}
-            </x-nav-link>
-        </div>
-
-
-
-
-
-{{--        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">--}}
-{{--            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">--}}
-{{--                <div class="p-6 bg-white border-b border-gray-200">--}}
-{{--                    You're logged in!--}}
-{{--                </div>--}}
-{{--            </div>--}}
+{{--        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex ">--}}
+{{--            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">--}}
+{{--                {{ __('Dashboard') }}--}}
+{{--            </x-nav-link>--}}
+{{--            <x-nav-link :href="route('home')" :active="request()->routeIs('home')">--}}
+{{--                {{ __('home') }}--}}
+{{--            </x-nav-link>--}}
+{{--            <x-nav-link :href="route('items')" :active="request()->routeIs('items')">--}}
+{{--                {{ __('Items') }}--}}
+{{--            </x-nav-link>--}}
+{{--            <x-nav-link :href="route('contactUs')" :active="request()->routeIs('contactUs')">--}}
+{{--                {{ __('Contact Us') }}--}}
+{{--            </x-nav-link>--}}
+{{--            <x-nav-link :href="route('aboutUs')" :active="request()->routeIs('aboutUs')">--}}
+{{--                {{ __('About Us') }}--}}
+{{--            </x-nav-link>--}}
 {{--        </div>--}}
+
+
+
+
+
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    You're logged in!
+                </div>
+            </div>
+        </div>
 
     </div>
 </x-app-layout>
