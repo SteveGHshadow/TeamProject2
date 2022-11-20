fa3a4747e5cdbca18d5f2b4c76751d6c88334033
SteveGHshadow <steve.itunesjp@hotmail.com>
2022-11-20 20:53:35 +0000

    Changed the nav bar and removed nav from dashboard

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -8,20 +8,12 @@
                     <a href="{{ route('dashboard') }}">
 {{--                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600 " />--}}
                         <x-application-logo class="absolute left-0 top-2 h-10 w-auto fill-current text-gray-600"/>
+                        <p class="p-3">Uni Style</p>
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex ">
-{{--                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">--}}
-{{--                        {{ __('Dashboard') }}--}}
-{{--                    </x-nav-link>--}}
-{{--                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">--}}
-{{--                        {{ __('home') }}--}}
-{{--                    </x-nav-link>--}}
-{{--                    <x-nav-link :href="route('items')" :active="request()->routeIs('items')">--}}
-{{--                        {{ __('Items') }}--}}
-{{--                    </x-nav-link>--}}
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
 
                     <form class="flex items-center">
                         <label for="simple-search" class="sr-only">Search</label>
@@ -31,11 +23,22 @@
                             </div>
                             <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required="">
                         </div>
-{{--                        <button type="submit" class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">--}}
-{{--                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>--}}
-{{--                            <span class="sr-only">Search</span>--}}
-{{--                        </button>--}}
+                        {{--                        <button type="submit" class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">--}}
+                        {{--                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>--}}
+                        {{--                            <span class="sr-only">Search</span>--}}
+                        {{--                        </button>--}}
                     </form>
+                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                            {{ __('Dashboard') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                            {{ __('home') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('items')" :active="request()->routeIs('items')">
+                            {{ __('Items') }}
+                        </x-nav-link>
+
+
 
                 </div>
             </div>
